856fce5d31f788cfd04d7a8e64c02b7904d83edc
Pablo <87919708+PMedico@users.noreply.github.com>
2023-05-08 16:37:26 -0300

    Delete practicaMetodos.ts

--- practicaMetodos.ts
+++
@@ -1,9 +0,0 @@
-import * as rls from "readline-sync"
-
-let mensaje: string = "hola global";
-ambitoVariables();
-
-function ambitoVariables(){
-  let mensaje:string;
-  mensaje = "Hola Mundo";
-  console.log(mensaje);
